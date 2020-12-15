058208c2b8857f1a721497785716260986abffc9
rob tillaart <rob.tillaart@gmail.com>
2020-12-15 08:11:53 +0100

    unit test

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -1,7 +1,7 @@
 //
 //    FILE: unit_test_001.cpp
 //  AUTHOR: Rob Tillaart
-//    DATE: 2020-12-03
+//    DATE: 2020-12-14
 // PURPOSE: unit tests for the BoolArray library
 //          https://github.com/RobTillaart/BoolArray
 //          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
@@ -35,10 +35,57 @@ unittest_teardown()
 
 unittest(test_constructor)
 {
-  BoolArray b;
-  assertEqual(0, b.size());
-  b.begin(1000);
-  assertEqual(1000, b.size());
+  BoolArray ba;
+  assertEqual(0, ba.size());
+  ba.begin(1000);
+  assertEqual(1000, ba.size());
+
+  fprintf(stderr, "\tVERSION:\t %s\n", BOOLARRAY_LIB_VERSION);
+
+}
+
+unittest(test_set_get_toggle)
+{
+  BoolArray ba;
+
+  ba.begin(1000);
+  assertEqual(BA_OK, ba.getError());
+
+  fprintf(stderr, "\t1000x set(i, 0) -> sum += get(i)\n");
+  int sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.set(i, 0);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(0, sum);
+
+  fprintf(stderr, "\t1000x set(i, 1) -> sum += get(i)\n");
+  sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.set(i, 1);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(1000, sum);
+  
+  fprintf(stderr, "\t1000x toggle(i)\n");
+  sum = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    ba.toggle(i);
+  }
+  for (int i = 0; i < 1000; i++)
+  {
+    sum += ba.get(i);
+  }
+  assertEqual(0, sum);
 }
 
 unittest_main()
